c1098d97c4eb3dbb23dfa5d0f7347d610c4122dc
Tracy <yu-tzu.wu2@mail.mcgill.ca>
2023-11-09 16:33:41 -0500

    Files removed

--- Functions_Indicators_Tracy/Compute_Spectral_Entropy.m
+++
@@ -1,9 +0,0 @@
-function SpecEnt = Compute_Spectral_Entropy(norm2_s1, Wave_FreqS);
-
-% norm2_s1=norm;
-
-SpecEnt = nan(size(norm2_s1,2),1) ;
-
-for iTime = 1:size(norm2_s1,2)
-    SpecEnt(iTime,1) = Spectral_Entropy(norm2_s1(:,iTime),Wave_FreqS);
-end
